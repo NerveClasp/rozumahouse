084a7d7a28e338dbf158c1754dd11d1ca52099f3
Roman Horokhov <nerveclasp@gmail.com>
2019-07-17 00:05:39 +0300

    remove svelte

--- svelte/src/apollo.js
+++
@@ -1,8 +0,0 @@
-import ApolloClient from 'apollo-boost';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-
-export const client = new ApolloClient({
-  uri: `http://${window.location.hostname}:8888/graphql`,
-  cache: new InMemoryCache(),
-});
